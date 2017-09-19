7c0da84c471c87118eadf348bb494d5e51ce0d63
Rob Campbell <git@raacampbell.com>
2017-09-19 15:52:34 +0200

    Update soma/projection plotter.

--- analysis/somaClustering/clusterPos.m
+++ analysis/somaClustering/clusterPos.m
@@ -4,6 +4,13 @@ function varargout = clusterPos(cellMat,cleanCells,highlightName)
     % Purpose
     % Plot the locations of somata that project to a given area
     %
+    % Inputs
+    % cellMat - the binary matrices and associated labels from Justus packages in structure.
+    %           if the structure is of length 2 then we assume the first index contains
+    %           the "good" cells from the main paper and the second index the premature
+    %           cells.
+    %
+    %
     % e.g.
     % >> load ~/tvtoucan/Mrsic-Flogel/hanyu/Analyses/cleanCells.mat
     % % The following loads "cellMat"
@@ -108,43 +115,24 @@ function varargout = clusterPos(cellMat,cleanCells,highlightName)
 
 
     % Plot soma positions
-
-    d = cleanCells.returnData;
-    details=cat(1,d.details);
-    cellID = {details.cellID};
-
-    % Find the index for each cell from the manuscript
-    for ii=1:length(cellMat.id)
-        f = strmatch(cellMat.id{ii},cellID);
-        cellMat.ind(ii) = f;
-
-        % Soma location in 25 micron voxel units
-        cellMat.somaPos{ii} = d(f).pointsInARA.rawSparseData.sparsePointMatrix(1,:);
-    end
-    pltDat = reshape([cellMat.somaPos{:}],3,[])';
-
-    plot(pltDat(:,2), pltDat(:,3), 'ob')
-
-
-    %Find cells projected to the highlighted area
-    if ~isempty(highlightName)
-        Hind = strmatch(highlightName,cellMat.lab);
-        if isempty(Hind)
-            fprintf('\nERROR: can not find area "%s" in cellmat.lab\n', highlightName)
-        end
-        Hcells = find(cellMat.mat(:,Hind));
-    end
-
-    plot(pltDat(Hcells,2), pltDat(Hcells,3), 'ob', 'MarkerFaceColor', [0.5,0.5,1])
-
-
-    hold off
-
     title(sprintf('Highlighting cells projecting to "%s"\n', highlightName));
     xlim([260,440])
     ylim([100,250]) 
 
+    %Remove non-V1 cells
+    S=scc.diagnostic.summariseTerminationTypes(cleanCells,true); %Second element is V1-only
+    V1cellsInds = S(2).indexValuesOfAllCells;
 
+    d = cleanCells.returnData;
+    d = d(V1cellsInds);
+    details=[d.details];
+
+    mSize=12;
+    plotSomaPositions(cellMat(1),d,details,highlightName,{'ob','MarkerSize',mSize},{'ob', 'MarkerFaceColor', [0.5,0.5,1],'MarkerSize',mSize})
+    if length(cellMat)>1
+        plotSomaPositions(cellMat(2),d,details,highlightName,{'sr','MarkerSize',mSize+1},{'sr', 'MarkerFaceColor', [1,0.5,0.5],'MarkerSize',mSize+1})
+    end
+    hold off
 
 
     if nargout>0
@@ -154,3 +142,54 @@ function varargout = clusterPos(cellMat,cleanCells,highlightName)
     if nargout>1
         varargout{2} = cellMat;
     end
+
+
+
+function plotSomaPositions(cellMat,cellData,details,highlightName,allCellsMarker,projectionCellsMarker)
+
+    cellID = {details.cellID};
+
+    % Find the index for each cell from the manuscript
+    for ii=length(cellMat.id):-1:1
+        f = strmatch(cellMat.id{ii},cellID);
+
+        if isempty(f)
+            fprintf('Skipping cell %s: it is present in the matrix from Justus but not in the filtered clean cells array.\n', cellMat.id{ii})
+            cellMat.id(ii)=[];
+            cellMat.mat(ii,:)=[];
+            continue
+        end
+
+        cellMat.ind(ii) = f;
+
+        % Soma location in 25 micron voxel units
+        cellMat.somaPos{ii} = cellData(f).pointsInARA.rawSparseData.sparsePointMatrix(1,:);
+    end
+
+
+    % Plot all cells
+    pltDat = reshape([cellMat.somaPos{:}],3,[])';
+    plot(pltDat(:,2), pltDat(:,3),allCellsMarker{:})
+
+
+    % Find cells that project to the highlighted area and overlay them
+    if ~isempty(highlightName)
+        Hind = strmatch(highlightName,cellMat.lab);
+        if isempty(Hind)
+            fprintf('Can not find area "%s" in cellmat.lab\n', highlightName)
+        else
+            Hcells = find(cellMat.mat(:,Hind)); % Index of cells with projections to this area
+            HcellIDs = cellMat.id(Hcells);      % IDs of cells with projections to this area;
+            for ii=1:length(HcellIDs)
+                tCell = HcellIDs{ii};
+                f=strmatch(tCell,{details.cellID});
+
+                if isempty(f)
+                    fprintf('Can not find cell %s in array: something is wrong!\n',tCell)
+                end
+                plot(pltDat(Hcells,2), pltDat(Hcells,3), projectionCellsMarker{:})
+
+            end
+        end
+    end
+
