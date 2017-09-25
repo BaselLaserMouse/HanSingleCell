47d26640d7947b05e8db4cbcf541b6d1a24e7379
Rob Campbell <git@raacampbell.com>
2017-09-25 16:57:45 +0200

    Return stats from clusterpos.

--- analysis/somaClustering/clusterPos.m
+++ analysis/somaClustering/clusterPos.m
@@ -1,5 +1,7 @@
 function varargout = clusterPos(cellMat,cleanCells,highlightName)
-    % function clusterPos(cellMat,cleanCells,highlightName)
+    % Highlight soma positions of cells projecting to a given brain area
+    %
+    % [plotHandles, dataTable] = clusterPos(cellMat,cleanCells,highlightName)
     %
     % Purpose
     % Plot the locations of somata that project to a given area
@@ -16,6 +18,10 @@ function varargout = clusterPos(cellMat,cleanCells,highlightName)
     % % The following loads "cellMat"
     % >> load goodCellsMatrixFromJustus.mat
     % >> clusterPos(cellMat,cleanCells,3)
+    %
+    %
+    % % Go through all projection targets and make an output table for each
+    % >> for ii=2:13, [~,dt{ii}]=clusterPos(allCellMat,cleanCells,ii); end
     %
     %
     % Rob Campbell - Basel 2017
@@ -76,8 +82,8 @@ function varargout = clusterPos(cellMat,cleanCells,highlightName)
     H.tRS=text(276,180,'RS (lateral)');
     set(H.tRS,'rotation',90, params{:})
 
-    H.tV1=text(330,165,'V1');
-    set(H.tV1,'rotation',0, params{:})
+    %H.tV1=text(330,165,'V1');
+    %set(H.tV1,'rotation',0, params{:})
 
     H.tA1=text(420,210,'A1');
     set(H.tA1,'rotation',0, params{:})
@@ -124,6 +130,24 @@ function varargout = clusterPos(cellMat,cleanCells,highlightName)
     xlim([260,440])
     ylim([100,250]) 
 
+
+    %Overlay the bissections lines 
+
+    r_h=111; %circle radius
+    c_x=442; %circle offset in x
+    c_y=177; %circle offset in y
+
+
+    x=linspace(0,1,50)+2.75; % X positions to take the sin and cos of for plotting
+    V1divmrkr = {'--','LineWidth',1.5,'Color',[1,1,1]*0.2};
+    plot(c_x+cos(x)*r_h, c_y+sin(x)*r_h, V1divmrkr{:})
+
+    %The coefs for the line
+    b0_v=87.0;
+    b1_v=0.2353;
+    x=[289,374];
+    plot(x, b0_v + x*b1_v, V1divmrkr{:})
+
     %Remove non-V1 cells
     S=scc.diagnostic.summariseTerminationTypes(cleanCells,true); %Second element is V1-only
     V1cellsInds = S(2).indexValuesOfAllCells;
@@ -134,25 +158,76 @@ function varargout = clusterPos(cellMat,cleanCells,highlightName)
     details=[d.details];
 
     mSize=12;
-    plotSomaPositions(cellMat(1),d,details,highlightName,{'ob','MarkerSize',mSize},{'ob', 'MarkerFaceColor', [0.5,0.5,1],'MarkerSize',mSize})
+    %Plot non-abrupt terminations
+    [cellLocs(1),cm(1)]=plotSomaPositions(cellMat(1),d,details,highlightName,{'ob','MarkerSize',mSize},...
+        {'ob', 'MarkerFaceColor', [0.5,0.5,1],'MarkerSize',mSize});
     if length(cellMat)>1
-        plotSomaPositions(cellMat(2),d,details,highlightName,{'sb','MarkerSize',mSize+1},{'sb', 'MarkerFaceColor', [0.5,0.5,1],'MarkerSize',mSize+1})
+        % Plot abrupt terminations if they are present
+        [cellLocs(2),cm(2)]=plotSomaPositions(cellMat(2),d,details,highlightName,{'sb','MarkerSize',mSize+1},...
+            {'sb', 'MarkerFaceColor', [0.5,0.5,1],'MarkerSize',mSize+1});
     end
-    hold off
+
+    % Now we make a table that contains the following columns
+
+    % 1. Cell ID (string)
+    % 2. Projection target name (string) - This will be the target for this run of clusterPos unless it's called with no arguments (in which case all are returned)
+    % 3. Medial or Lateral ('M' or 'L') side of the border
+    % 4. Rostral or caudal ('R' or 'C') side of the border
+    % 5. Is premature? [bool]
+    % 6. Soma position
+
+    IDs = cat(2,cm.id)';
+    L = length(IDs);
+
+    %Figure out on which side of each boundary each cell falls;
+    %Cells within the circle diamater will be lateral so make all medial by default
+    ML = repmat({'M'},L,1);
+
+    pos = cat(1,cellLocs.pos);
+
+    d = squareform(pdist([c_x,c_y;pos])); %Distance of all WRT circle centre
+    d = d(1,2:end);
+    lateralInds = find(d<=r_h);
+    ML(lateralInds) = repmat({'L'},1,length(lateralInds));
 
 
+    %Cells beneath the line will be caudal. Make this the default then figure out what is rostral.
+    RC = repmat({'C'},L,1);
+    border=b0_v + pos(:,1)*b1_v;
+    rostralInds = find((pos(:,2)-border)>0);
+    RC(rostralInds) = repmat({'R'},1,length(rostralInds));
+
+    %Cells that project to the area in question
+    proj = repmat({''},L,1);
+    if ~isempty(highlightName)
+        projCells = cat(2,cellLocs.projectingCellID)';
+        for ii=1:length(projCells)
+            f=find(strcmp(projCells{ii},IDs));
+            proj{f} = highlightName;
+        end
+    end
+
+    out = table(IDs, ...
+        proj, ...
+        ML, ...
+        RC, ...
+        [repmat(false,length(cm(1).id),1); repmat(true,length(cm(2).id),1)], ...
+        pos);
+    out.Properties.VariableNames = {'CellID','Target','ML','RC','isPremature','position'};
 
     if nargout>0
         varargout{1} = H;
     end
 
     if nargout>1
-        varargout{2} = cellMat;
+        varargout{2} = out;
     end
 
+    hold off
 
 
-function plotSomaPositions(cellMat,cellData,details,highlightName,allCellsMarker,projectionCellsMarker)
+function [somaLocations,cellMat] = plotSomaPositions(cellMat,cellData,details,highlightName,allCellsMarker,projectionCellsMarker)
+    % return cellMat because some cells might have been removed from it. 
 
     cellID = {details.cellID};
 
@@ -173,12 +248,14 @@ function plotSomaPositions(cellMat,cellData,details,highlightName,allCellsMarker
         cellMat.somaPos{ii} = cellData(f).pointsInARA.rawSparseData.sparsePointMatrix(1,:);
     end
 
-
     % Plot all cells
     pltDat = reshape([cellMat.somaPos{:}],3,[])';
     plot(pltDat(:,2), pltDat(:,3),allCellsMarker{:})
 
-
+    somaLocations.allCells = [(1:length(pltDat))' , pltDat(:,2:3)];
+    somaLocations.projectingCells = [];
+    somaLocations.pos = pltDat(:,2:3);
+    somaLocations.projectingCellID ={};
     % Find cells that project to the highlighted area and overlay them
     if ~isempty(highlightName)
         Hind = strmatch(highlightName,cellMat.lab);
@@ -193,10 +270,14 @@ function plotSomaPositions(cellMat,cellData,details,highlightName,allCellsMarker
 
                 if isempty(f)
                     fprintf('Can not find cell %s in array: something is wrong!\n',tCell)
+                    continue
                 end
-                plot(pltDat(Hcells,2), pltDat(Hcells,3), projectionCellsMarker{:})
 
+                tmp = [Hcells(ii),pltDat(Hcells(ii),2:3)];
+                plot(tmp(:,2), tmp(:,3), projectionCellsMarker{:})
+
+                somaLocations.projectingCells = [somaLocations.projectingCells; tmp];
             end
+            somaLocations.projectingCellID = HcellIDs;
         end
     end
-
